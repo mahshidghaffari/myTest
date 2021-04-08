74b07d96681ca08b7472fbc25766306228149e09
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2021-04-08 09:35:21 +0200

    first attempt

---
+++ JavaTest.java
@@ -0,0 +1,12 @@
+
+public class JavaTest {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        	System.out.println("this jsut fucking test for working on the git and terminal");
+	}
+	
+	
+
+}
